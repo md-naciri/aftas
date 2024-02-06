792daf3cd2131e238b02ccc4cbcc56b4632284dd
md_naciri <med.ar.naciri@gmail.com>
2024-02-06 14:23:58 +0100

    create Role (enum), AppUser class

--- Backend/src/main/java/com/example/aftas/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.aftas.domain;
-
-public enum Role {
-    USER,
-    ADMIN
-}
---
+++ Backend/src/main/java/com/example/aftas/domain/enumeration/Role.java
@@ -0,0 +1,6 @@
+package com.example.aftas.domain.enumeration;
+
+public enum Role {
+    USER,
+    ADMIN
+}
